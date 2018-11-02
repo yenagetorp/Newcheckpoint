ceda905bbed1d53e5f4755ca84c37eaf8946cf8c
Yen Agetorp <yen.agetorp@gmail.com>
2018-11-02 15:26:39 +0100

    wrote hello world in the consolen

--- Checkpoint004/Program.cs
+++ Checkpoint004/Program.cs
@@ -6,7 +6,7 @@ namespace Checkpoint004
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
