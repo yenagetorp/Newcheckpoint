a382beb81cf67d6bae228e010e2f32237097cbdc
Yen Agetorp <yen.agetorp@gmail.com>
2018-11-02 15:29:41 +0100

    wrote best movies ever

--- Checkpoint004/Program.cs
+++ Checkpoint004/Program.cs
@@ -7,6 +7,7 @@ namespace Checkpoint004
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Best movies ever!");
         }
     }
 }
