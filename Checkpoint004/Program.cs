ccb86a3124d0e412d6c2e3d27a24991a5f16c0f5
Yen Agetorp <yen.agetorp@gmail.com>
2018-11-02 16:24:07 +0100

    display file names

--- Checkpoint004/Program.cs
+++ Checkpoint004/Program.cs
@@ -8,10 +8,9 @@ namespace Checkpoint004
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("Best movies ever!");
+            Console.WriteLine("Filmname:First of Danger");
             Console.WriteLine("Filmname: Master of Extermination");
             Console.WriteLine("Copyright < Yen Agetorp > 2018 :)" ); 
-            Console.WriteLine("Filmname:First of Danger");
-            Console.WriteLine("Copyright < ditt namn > 2018 :)" ); 
         }
     }
 }
